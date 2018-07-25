0015e6ae162d356200808940bf0a0df09edddad2
Phil Howard <phil@gadgetoid.com>
2018-07-25 09:13:36 +0000

    Test temperature

--- library/test.py
+++ library/test.py
@@ -1,1 +1,14 @@
-import lsm303d
+import time
+import lsm303d as lsm
+
+while True:
+    t = lsm.temperature()
+    m = ''
+    a = ''
+    h = 0
+    t_raw = lsm._lsm303d.values['TEMPERATURE']
+    #m = lsm.magnetometer()
+    #a = lsm.accelerometer()
+    #h = lsm.heading()
+    print("{:04.1f} {:016b}".format(t, t_raw))
+    time.sleep(1)
