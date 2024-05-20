015a27f101253cbf6564d9799ed1cf5401c8ff88
Phil Howard <phil@gadgetoid.com>
2024-05-20 17:42:34 +0100

    QA: Update tests.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,22 +1,12 @@
-import sys
-
-import mock
 import pytest
 
 
-def test_setup_not_present():
-    sys.modules['smbus'] = mock.MagicMock()
-    from lsm303d import LSM303D
+def test_setup_not_present(smbus_not_present, LSM303D):
     lsm303d = LSM303D()
     with pytest.raises(RuntimeError):
         lsm303d.setup()
 
 
-def test_setup_mock_present():
-    from tools import SMBusFakeDevice
-    smbus = mock.Mock()
-    smbus.SMBus = SMBusFakeDevice
-    sys.modules['smbus'] = smbus
-    from lsm303d import LSM303D
+def test_setup_mock_present(smbus, LSM303D):
     lsm303d = LSM303D()
     lsm303d.setup()
